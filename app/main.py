86790d59124e622c902ad81f1111c9c9afbc7d8a
CYX22222003 <e1155533@u.nus.edu>
2024-12-04 00:19:22 +0800

    Retrigger test

--- app/main.py
+++ app/main.py
@@ -68,7 +68,7 @@ def scan(file_contents):
                     shouldPrint = False
                 else:
                     token = "SLASH"
-            case "\t" | "\r" | " " :
+            case "\t" | "\r" | " " | "\n":
                 shouldPrint = False
             case _:
                 error = True
