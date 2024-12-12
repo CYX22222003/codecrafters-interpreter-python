ad167d89192a52ca45fbd02e7adea3b427e9a1b4
CYX22222003 <e1155533@u.nus.edu>
2024-12-12 10:54:48 +0800

    Add more native function

--- app/environment/environment.py
+++ app/environment/environment.py
@@ -1,17 +1,19 @@
+from app.environment.predefined import indexAt, makeList, scanin, toint, tostr, xsLength
 from app.exception.exceptions import LoxRuntimeException
 from app.lexer.token import Token
 import time
 
+
 class Environment:
-    def __init__(self, env = None):
+    def __init__(self, env=None):
         self.values = {}
         self.hashSet = set({})
         self.enclosing = env
-        
+
     def put(self, name: str, val):
         self.values[name] = val
         self.hashSet.add(name)
-        
+
     def update(self, t: Token, val):
         name = t.lexeme
         if name not in self.hashSet:
@@ -20,20 +22,29 @@ class Environment:
             else:
                 self.enclosing.update(t, val)
                 return
-        self.values[name] = val;
-    
+        self.values[name] = val
+
     def get(self, t: Token):
         if t.lexeme in self.hashSet:
             return self.values.get(t.lexeme)
         elif self.enclosing != None:
-            return self.enclosing.get(t)    
+            return self.enclosing.get(t)
         raise LoxRuntimeException(t, "Undefined variable '" + t.lexeme + "'.")
-    
+
     def extend(self, new_enclosse):
         self.enclosing = new_enclosse
 
+
 class Global(Environment):
     def __init__(self):
-        self.values = {"clock" : lambda : time.time()}
-        self.hashSet = {"clock"}
+        self.values = {
+            "clock": lambda: time.time(),
+            "scanin": scanin,
+            "toint": toint,
+            "tostr": tostr,
+            "makeList" : makeList,
+            "indexAt" : indexAt,
+            "xsLength" : xsLength
+        }
+        self.hashSet = set(self.values.keys())
         self.enclosing = None
